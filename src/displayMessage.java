1be43e7ba2a1be69378e1b721b1e8f48c5f5bc15
Andrzej Rzeszut <andrzejrz556@gmail.com>
2020-01-12 13:05:30 +0100

    Create method which is getting message to display from user

--- src/displayMessage.java
+++ src/displayMessage.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 public class displayMessage {
 
     public static void showMessage(String message){
         System.out.println(message);
+    }
+
+    public static String getMessageFromUser(){
+        System.out.println("Enter message to print: ");
+        Scanner sc = new Scanner(System.in);
+        String message = sc.nextLine();
+        return message;
     }
 }
